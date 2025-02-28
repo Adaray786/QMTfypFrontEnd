148d6154269d96db3a8c19fed1f5d948687b0347
Adam Uddin <adz@Adams-MacBook-Pro.local>
2025-02-28 01:29:24 +0000

    QMT-0017: Created functionality to show ayah progress for each surah

--- src/controller/SurahDetailsController.ts
+++ src/controller/SurahDetailsController.ts
@@ -4,15 +4,31 @@ import axios from "axios";
 export class SurahDetailsController {
     public static async getSurahDetails(req: Request, res: Response): Promise<void> {
         const surahId = req.params.id;
+        const userId = req.session.user?.userId;
 
         try {
             const surahResponse = await axios.get(process.env.BACK_URL + `/api/surahs/${surahId}`);
 
             const ayahResponse = await axios.get(process.env.BACK_URL + `/api/ayahs/surah/${surahId}`);
 
+            let memorizedAyahs: number[] = [];
+
+            if (userId) {
+                // Fetch user's memorized Ayahs
+                const ayahProgressResponse = await axios.get(process.env.BACK_URL + `/api/ayahProgress/${userId}/surah/${surahId}`);
+
+                // Ensure data is an array before filtering
+                const progressData = Array.isArray(ayahProgressResponse.data) ? ayahProgressResponse.data : [];
+
+                memorizedAyahs = progressData
+                    .filter((progress: any) => progress.memorized)
+                    .map((progress: any) => progress.ayahId);
+            }
+
             res.render("surah", { 
                 surah: surahResponse.data, 
                 ayahs: ayahResponse.data, 
+                memorizedAyahs,
                 user: req.session.user 
             });
         } catch (e) {
